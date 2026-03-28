45f200da315a0c955b1e7e517f4f20e629d7f0ea
yeoniii <jgy3445@gmail.com>
2026-03-28 20:29:47 +0900

    2주차 기능구현 완료

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django. contrib.auth import views as auth_views
-from .views import signup_view, home_view
+from .views import signup_view, home_view, find_password_view, reset_password_view
+
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),    #함수뷰
@@ -9,4 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(    #클래스뷰
         template_name='registration/login.html'
     ), name='login'),
+
+    path('Find_pw/', find_password_view, name='find_password'),
+    path('Reset_pw/', reset_password_view, name='reset_password'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django. contrib.auth import login
 from django. contrib. auth. decorators import login_required
 from .forms import SignUpForm
+from .models import CustomUser
 
 def signup_view(request):
     if request.method == 'POST':
@@ -20,8 +21,46 @@ def signup_view(request):
 
     return render(request, 'signup.html', {'form': form})
 
-
-
 @login_required
 def home_view(request):
     return render(request, 'home.html')
+
+def find_password_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        phone_number = request.POST.get('phone_number')
+
+        try:
+            user = CustomUser. objects.get(username=username, phone_number=phone_number)
+            request.session['reset_password_id'] = user.id
+            return redirect('reset_password')
+        
+        except CustomUser.DoesNotExist:
+          return render(request, 'find_pw.html', {
+            'error': '아이디 혹은 전화번호가 일치하지 않습니다.'
+          })
+        
+    return render(request, 'find_pw.html')
+        
+
+def reset_password_view(request):
+    user_id = request.session.get('reset_password_id')
+
+    if not user_id:
+        return redirect('find_password')
+    
+    user = CustomUser.objects.get(id=user_id)
+
+    if request. method == 'POST':
+       new_password = request.POST.get('password')
+
+       user.set_password(new_password)
+       user.save()
+
+       del request. session['reset_password_id']
+       return redirect ('login')
+    
+    return render(request, 'reset_pw.html')
+
+
+    
